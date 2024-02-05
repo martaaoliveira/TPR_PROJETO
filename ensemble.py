1d375be523e12aefd82d8946490913becb7958cc
Marta Oliveira <marta.alex@ua.pt>
2024-02-05 19:19:55 +0000

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-from profiling import one_class_svm, one_class_svm_with_pca, isolation_forest, isolation_forest_with_pca, lof_classification, lof_classification_with_pca
+from profiling import waitforEnter, one_class_svm, one_class_svm_with_pca, isolation_forest, isolation_forest_with_pca, lof_classification, lof_classification_with_pca,gmm_classification,gmm_classification_with_pca
 from sklearn.metrics import confusion_matrix, f1_score
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -53,18 +53,19 @@ def main():
     for i in range(nObsTest):
         actual_labels.append(o3testClass[i][0])
 
-    labels1 = one_class_svm(trainFeatures_bruno, testFeatures_bruno, testFeatures_dns, o3testClass,name_excel)
-    # labels2 = one_class_svm_with_pca(trainFeatures_bruno, testFeatures_bruno, testFeatures_dns, o3testClass,name_excel)
-    labels3 = isolation_forest(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass, name_excel)
-    labels4 = isolation_forest_with_pca(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
-    labels5 = lof_classification(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
-    labels6 = lof_classification_with_pca(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
-
-    # lables_list = [labels1, labels2, labels3, labels4, labels5, labels6]
-    lables_list = [labels1, labels3, labels4, labels5, labels6]
+    #labels1 = one_class_svm(trainFeatures_bruno, testFeatures_bruno, testFeatures_dns, o3testClass,name_excel)
+    #labels2 = one_class_svm_with_pca(trainFeatures_bruno, testFeatures_bruno, testFeatures_dns, o3testClass,name_excel)
+    #labels3 = isolation_forest(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass, name_excel)
+    #labels4 = isolation_forest_with_pca(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
+    #labels5 = lof_classification(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
+    #labels6 = lof_classification_with_pca(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
+    labels7=gmm_classification(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
+    labels8=gmm_classification_with_pca(trainFeatures_bruno,testFeatures_bruno, testFeatures_dns,o3testClass,name_excel)
+    lables_list = [labels7, labels8]
+    #lables_list = [labels1, labels2,labels3, labels4, labels5,labels7,labels8]
     final_lables = []
 
-    for i in range(len(labels1)):
+    for i in range(len(labels7)):
         cnt = 0
         for j in range(len(lables_list)):
             if (lables_list[j][i] == 0):
@@ -78,6 +79,7 @@ def main():
 
     confusion_matrix_result = confusion_matrix(actual_labels, final_lables)
     f1 = f1_score(actual_labels, final_lables, pos_label=2)
+    f1=round(f1, 2)
     print("F1 Score Ensemble: ", f1)
 
     # Plot the confusion matrix
@@ -86,37 +88,10 @@ def main():
                 xticklabels=['Normal', 'DNS TUNNEL'], yticklabels=['Normal', 'DNS TUNNEL'])
     plt.xlabel('Predicted')
     plt.ylabel('Actual')
-    plt.title('Confusion Matrix Local Ensemble with F1 score of {f1}')
+    plt.title(f'Confusion Matrix Local Ensemble with F1 score of {f1}')
     plt.show()
+    waitforEnter(fstop=True)
 
-
-
-    # directory_path = './resultados_script_dumb'
-    # excel_files = get_excel_files(directory_path)
-
-    # all_metrics = []
-
-    # for file_path in excel_files:
-    #     df = pd.read_excel(file_path)
-    #     TN, FP, FN, TP = get_metrics(df)
-    #     all_metrics.append({'TN': TN, 'FP': FP, 'FN': FN, 'TP': TP})
-
-    # print(all_metrics)
-
-    # total_TN = sum(metric['TN'] for metric in all_metrics)
-    # total_FP = sum(metric['FP'] for metric in all_metrics)
-    # total_FN = sum(metric['FN'] for metric in all_metrics)
-    # total_TP = sum(metric['TP'] for metric in all_metrics)
-
-    # total_metrics= total_TN+ total_FP+total_FN+total_TP
-    # #print(total_metrics)
-    # pred_final = (total_metrics)/len(excel_files)
-    # #print(pred_final)
-    # # Check if more than half of the algorithms indicate a True Positive
-    # if pred_final >total_metrics/2:
-    #     print("More than half of the algorithms indicate a True Positive. Possible attack detected!")
-    # else:
-    #     print("No attack detected.")
 
 if __name__ == "__main__":
     main()
